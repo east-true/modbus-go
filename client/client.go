6e15cab491d8a5c80ad3438b512d294a2552a015
east-true <leik24@naver.com>
2023-07-28 17:35:45 +0900

    merge rtu and tcp to client

--- client/client.go
+++ client/client.go
@@ -1,10 +1,16 @@
-package handler
+package client
 
 import (
 	"time"
 
 	"github.com/goburrow/modbus"
 )
+
+type Client interface {
+	GetClient() modbus.Client
+	Connect() error
+	Close() error
+}
 
 type RTU struct {
 	SlaveID  byte
@@ -30,6 +36,10 @@ func NewRTU() *RTU {
 	}
 }
 
+func (rtu *RTU) GetClient() modbus.Client {
+	return rtu.c
+}
+
 func (rtu *RTU) SetHandler() {
 	rtu.h = modbus.NewRTUClientHandler(rtu.Address)
 	rtu.h.BaudRate = rtu.BaudRate
@@ -53,6 +63,41 @@ func (rtu *RTU) Close() error {
 	return rtu.h.Close()
 }
 
-func (rtu *RTU) Client() modbus.Client {
-	return rtu.c
+type TCP struct {
+	Address string
+	SlaveID byte
+	Timeout time.Duration
+	h       *modbus.TCPClientHandler
+	c       modbus.Client
+}
+
+func NewTCP() *TCP {
+	return &TCP{
+		Address: "127.0.0.1:113",
+		SlaveID: 1,
+		Timeout: 5,
+	}
+}
+
+func (tcp *TCP) GetClient() modbus.Client {
+	return tcp.c
+}
+
+func (tcp *TCP) SetHandler() {
+	tcp.h = modbus.NewTCPClientHandler(tcp.Address)
+	tcp.h.SlaveId = tcp.SlaveID
+	tcp.h.Timeout = tcp.Timeout
+}
+
+func (tcp *TCP) Connect() error {
+	if err := tcp.h.Connect(); err != nil {
+		return err
+	}
+
+	tcp.c = modbus.NewClient(tcp.h)
+	return nil
+}
+
+func (tcp *TCP) Close() error {
+	return tcp.h.Close()
 }
