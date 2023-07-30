47c28d39a4dbad8858ff0f90532cddb0adf74996
east-true <leik24@naver.com>
2023-07-30 22:46:31 +0900

    add test code

---
+++ modbus_test.go
@@ -0,0 +1,21 @@
+package modbus_test
+
+import (
+	"testing"
+
+	"github.com/east-true/goutil/parser"
+	. "github.com/east-true/modbus-go"
+	"github.com/east-true/modbus-go/memory"
+)
+
+func TestTCP(t *testing.T) {
+	mem := memory.New(memory.FUNC_READ_HOLDING_REGISTERS, 0, parser.LITTLE_LOWER, parser.INT32ARR, 1)
+	tcp := NewTCP(mem)
+	if err := tcp.Connect(); err != nil {
+		t.Error(err)
+	} else {
+		defer tcp.Close()
+	}
+
+	tcp.Read()
+}
